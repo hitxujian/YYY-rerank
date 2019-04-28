1c9a1e0a6079229e252af2037bb7f459b8a7d45b
PAN001 <pan.atopos@gmail.com>
2019-04-28 13:40:39 -0400

    test

--- rerank.sh
+++ rerank.sh
@@ -15,6 +15,7 @@ python exp.py \
     --decode_max_time_step 100 \
     --train_file ${train_file} \
     --dev_file ${dev_file} \
+    --test_file ${test_file} \
     --evaluator conala_evaluator \
     --asdl_file asdl/lang/py3/py3_asdl.simplified.txt \
     --lang python3 \
