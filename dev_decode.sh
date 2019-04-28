816a3f30bc8dad9b8de3cdf75e73266fdf358b83
PAN001 <pan.atopos@gmail.com>
2019-04-28 13:24:08 -0400

    test

--- dev_decode.sh
+++ dev_decode.sh
@@ -2,14 +2,14 @@
 
 source activate py3torch3cuda9
 
-test_file="data/conala/test.var_str_sep.bin"
+dev_file="data/conala/dev.var_str_sep.bin"
 
 python exp.py \
     --cuda \
     --mode test \
     --load_model $1 \
     --beam_size 15 \
-    --test_file ${test_file} \
+    --test_file ${dev_file} \
     --evaluator conala_evaluator \
     --save_decode_to decodes/conala/$(basename $1).dev.decode \
     --decode_max_time_step 100
